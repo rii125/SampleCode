1b8c6d837fcb466f7744b9eb6faa997c54cb53f8
rii_125 <rii125.idd@gmail.com>
2025-03-30 12:52:42 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main {
+    int[] array1 = {1, 10, 1000};
+    int[] array2;
+    array2 = array1;
+    array2[0] = 100;
+    System.out.println(array1[0]);
+  }
+}
